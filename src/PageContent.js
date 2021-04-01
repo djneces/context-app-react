fc2352446befc1834df30c6c13d70231d4a4d852
djneces <j.necesanek@hotmail.com>
2021-04-01 10:30:43 +0200

    useContext hook & transform to func components

--- src/PageContent.js
+++ src/PageContent.js
@@ -1,19 +1,14 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from './contexts/ThemeContext';
 
-export class PageContent extends Component {
-  //consuming context
-  static contextType = ThemeContext;
-  render() {
-    //consuming context
-    const { isDarkMode } = this.context;
-    const styles = {
-      backgroundColor: isDarkMode ? 'black' : 'white',
-      height: '100vh',
-      width: '100vw',
-    };
-    return <div style={styles}>{this.props.children}</div>;
-  }
+export function PageContent(props) {
+  const { isDarkMode } = useContext(ThemeContext);
+  const styles = {
+    backgroundColor: isDarkMode ? 'black' : 'white',
+    height: '100vh',
+    width: '100vw',
+  };
+  return <div style={styles}>{props.children}</div>;
 }
 
 export default PageContent;
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,27 +1,16 @@
-import React, { createContext, Component } from 'react';
+import React, { createContext } from 'react';
+import useToggleState from '../hooks/useToggleState';
 
 export const ThemeContext = createContext();
 
 //we give it data what we consume in another components ->children
-export class ThemeProvider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isDarkMode: false };
-    this.toggleTheme = this.toggleTheme.bind(this);
-  }
+export function ThemeProvider(props) {
+  const [isDarkMode, toggleTheme] = useToggleState(false);
 
-  toggleTheme() {
-    console.log(this);
-    this.setState({ isDarkMode: !this.state.isDarkMode });
-  }
-  render() {
-    //one value per context - what is passed to children
-    return (
-      <ThemeContext.Provider
-        value={{ ...this.state, toggleTheme: this.toggleTheme }}
-      >
-        {this.props.children}
-      </ThemeContext.Provider>
-    );
-  }
+  //one value per context - what is passed to children
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
 }
